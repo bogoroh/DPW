378ebbdf05b347f584b0ad671e37de3ee052b447
bogoroh <mtaatgen@live.com>
2014-01-06 22:54:33 -0500

    Fixed input with raw_input
    raw_input actually reads input as a string so we won’t have to type ine
    the quotation marks

--- madlib.py
+++ madlib.py
@@ -1,20 +1,18 @@
-name = input("What is your name? Remember to type in Quotes") #String
-day = input("What day is it? Remember to type in Quotes") #String
-sunny = input("Is it sunny? (Yes/No) Remember to type in Quotes") #String
-currency = input("What is the current currency of Euro to US Dollar? (Hint=1.36) ") #Float 
-money = input("How much money did he find? Please fill in a whole number in Euros") #Number
+name = raw_input("What is your name? Remember to type in Quotes") #String
+day = raw_input("What day is it? Remember to type in Quotes") #String
+sunny = raw_input("Is it sunny? (Yes/No) Remember to type in Quotes") #String
+currency = float(raw_input("What is the current currency of Euro to US Dollar? (Hint=1.36) ")) #Float 
+money = int(raw_input("How much money did he find? Please fill in a whole number in Euros")) #Number
 
 #Ammount he found in US Dollar
-int(money)
-float(currency)
 dollar = money * currency
-float(dollar)
+
 
 #If statement for the opening sentence to check if it's sunny or not
 if sunny == "Yes":
-    opening = "It's a sunny"
+    opening = "It was a sunny"
 else:
-    opening = "It's a grimy"
+    opening = "It was a grimy"
     
 #Casino Game
 casinoGame = ["Poker","Roulette","Blackjack","Slots","Craps"]
@@ -22,7 +20,7 @@ casinoGame = ["Poker","Roulette","Blackjack","Slots","Craps"]
 #Bartender 
 bartender = {"Vanessa":"1800 Silver Dog","Britney":"Abbot's Dream","Jessica":"Apple Daiquiri","Nicole":"Bangkok Bomb"}
 
-message = '''{opening} {day}. The ducks were swimming and it looked like a nomal day at work for {name}. After work {name} was walking to his car and found a wallet with {money} Euro in it. The following day he went to the bank and converted his Euros into dollar with the rate of {currency} per euro.{name} got {dollar} out of it and decided to go the casino and try his luck with the money he found. When he arrived at the casino he got greeted by his favorite bartender Vanessa with her favorite drink called: bartender["Vanessa"].    '''
+message = '''{opening} {day}. The ducks were swimming and it looked like a nomal day at work for {name}. After work {name} was walking to his car and found a wallet with {money} Euro in it. The following day he went to the bank and converted his Euros into dollars with the rate of {currency} per Euro. {name} got {dollar} out of it and decided to go the casino and try his luck with the money he found. When he arrived at the casino he got greeted by his favorite bartender Vanessa with her favorite drink called: bartender["Vanessa"].'''
 messageFormatted = message.format(**locals())
 
 print messageFormatted
