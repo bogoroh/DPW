41d5b915dd7783d037b1025122ae9a29c45ab330
bogoroh <mtaatgen@live.com>
2014-01-06 21:49:21 -0500

    Made the first if statement(sunny)

---
+++ madlib.py
@@ -0,0 +1,16 @@
+day = input("What day is it? Remember to type in Quotes")
+str(day)
+print name
+
+sunny = input("Is it daytime? (Yes/No) Remember to type in Quotes")
+str(sunny)
+
+if sunny == "Yes":
+    opening = "It's a sunny"
+else:
+    opening = "It's a grimy"
+    
+print opening
+
+
+#var - It's a sunny or it's a grimy 
