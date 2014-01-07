742fce9738e4e9aadc9fff9ce57779439d94782e
bogoroh <mtaatgen@live.com>
2014-01-06 22:09:49 -0500

    took out str & int

--- madlib.py
+++ madlib.py
@@ -1,11 +1,10 @@
 name = input("What is your name? Remember to type in Quotes")
-str(name)
 
 day = input("What day is it? Remember to type in Quotes")
-str(day)
+
+money = input("How much money did he find? Please fill in a whole number")
 
 sunny = input("Is it sunny? (Yes/No) Remember to type in Quotes")
-str(sunny)
 
 #If statement for the opening sentence to check if it's sunny or not
 if sunny == "Yes":
@@ -17,6 +16,9 @@ else:
 casinoGame = ["Poker","Roulette","Blackjack","Slots","Craps"]
     
     
-print opening
 
-'''{opening} {day}. The ducks were swimmming and it looked like a nomal day at work for {name}.  ''' 
+
+message = '''{opening} {day}. The ducks were swimmming and it looked like a nomal day at work for {name}. After work {name} was walking to his car and found a wallet with ${money} in it. {name} Decided to go the casino and try his luck with the money he found   '''
+messageFormatted = message.format(**locals())
+
+print messageFormatted
