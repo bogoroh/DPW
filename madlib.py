ab2c3372ab7a6608e7dba51002f40ee5d1e46657
bogoroh <mtaatgen@live.com>
2014-01-06 21:54:40 -0500

    Fixed daytime with sunny & added name

--- madlib.py
+++ madlib.py
@@ -1,9 +1,13 @@
+name = input("What is your name? Remember to type in Quotes")
+str(name)
+
 day = input("What day is it? Remember to type in Quotes")
 str(day)
 
-sunny = input("Is it daytime? (Yes/No) Remember to type in Quotes")
+sunny = input("Is it sunny? (Yes/No) Remember to type in Quotes")
 str(sunny)
 
+#If statement for the opening sentence to check if it's sunny or not
 if sunny == "Yes":
     opening = "It's a sunny"
 else:
@@ -11,5 +15,4 @@ else:
     
 print opening
 
-
-#var - It's a sunny or it's a grimy 
+'''{opening} {day}. The ducks were swimmming and it looked like a nomal day at work for {name}.  ''' 
