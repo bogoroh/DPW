b893e54fbe8bc17913eed39c2bd5afcb5d74cc6b
bogoroh <mtaatgen@live.com>
2014-01-06 22:32:33 -0500

    fixed bartender name in message

--- madlib.py
+++ madlib.py
@@ -1,9 +1,8 @@
-name = input("What is your name? Remember to type in Quotes")
-
-day = input("What day is it? Remember to type in Quotes")
-sunny = input("Is it sunny? (Yes/No) Remember to type in Quotes")
-currency = input("What is the current currency of Euro to US Dollar? (Hint=1.36) ")
-money = input("How much money did he find? Please fill in a whole number in Euros")
+name = input("What is your name? Remember to type in Quotes") #String
+day = input("What day is it? Remember to type in Quotes") #String
+sunny = input("Is it sunny? (Yes/No) Remember to type in Quotes") #String
+currency = input("What is the current currency of Euro to US Dollar? (Hint=1.36) ") #Float 
+money = input("How much money did he find? Please fill in a whole number in Euros") #Number
 
 #Ammount he found in US Dollar
 dollar = money * currency
@@ -20,7 +19,7 @@ casinoGame = ["Poker","Roulette","Blackjack","Slots","Craps"]
 #Bartender 
 bartender = {"Vanessa":"1800 Silver Dog","Britney":"Abbot's Dream","Jessica":"Apple Daiquiri","Nicole":"Bangkok Bomb"}
 
-message = '''{opening} {day}. The ducks were swimmming and it looked like a nomal day at work for {name}. After work {name} was walking to his car and found a wallet with {money} Euro in it. The following day he went to the bank and converted his Euros into dollar with the rate of {currency} per euro.{name} got {dollar} out of it and decided to go the casino and try his luck with the money he found. When he arrived at the casino he got greeted by his favorite bartender vanessa with her favorite drink called: bartender["Vannessa"].    '''
+message = '''{opening} {day}. The ducks were swimmming and it looked like a nomal day at work for {name}. After work {name} was walking to his car and found a wallet with {money} Euro in it. The following day he went to the bank and converted his Euros into dollar with the rate of {currency} per euro.{name} got {dollar} out of it and decided to go the casino and try his luck with the money he found. When he arrived at the casino he got greeted by his favorite bartender vanessa with her favorite drink called: bartender["Vanessa"].    '''
 messageFormatted = message.format(**locals())
 
-#print messageFormatted
+print messageFormatted
