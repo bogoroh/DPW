e4b4c3a594d8e698c849df65897de5c86b7ba78e
bogoroh <mtaatgen@live.com>
2014-01-13 23:24:32 -0500

    A- 5 Data Objects

--- Lab3/grade-avg/main.py
+++ Lab3/grade-avg/main.py
@@ -6,8 +6,56 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+		mike = Person(5555) # Password for his/her voicemail
+		mike.name = "Mike Taatgen" # Name of the user
+		mike.text = 45 #Amount of text send
+		mike.minutes = 300 # Amount of minutes talked on the phone
+		mike.internet =  2 # Amount of GB used for data
+		
+		anthony = Person(5235) # Password for his/her voicemail
+		anthony.name = "Anthony Kluba" # Name of the user
+		anthony.text = 25 #Amount of text send
+		anthony.minutes = 325 # Amount of minutes talked on the phone
+		anthony.internet = 2.2 # Amount of GB used for data
+
+		nate = Person(1821) # Password for his/her voicemail
+		nate.name = "Nathan Dickison" # Name of the user
+		nate.text = 75 #Amount of text send
+		nate.minutes = 290 # Amount of minutes talked on the phone
+		nate.internet = 3.2 # Amount of GB used for data
+
+		jairo = Person(8371) # Password for his/her voicemail
+		jairo.name = "Jairo Jurado" # Name of the user
+		jairo.text = 25 #Amount of text send
+		jairo.minutes = 400 # Amount of minutes talked on the phone
+		jairo.internet =  1 # Amount of GB used for data
+		
+		rebecca = Person(9213) # Password for his/her voicemail
+		rebecca.name = "Rebecca Carroll" # Name of the user
+		rebecca.text = 49 #Amount of text send
+		rebecca.minutes = 280 # Amount of minutes talked on the phone
+		rebecca.internet = 4 # Amount of GB used for data
+
+		
+		players = [mike,anthony,nate,jairo,rebecca]
+		#self.response.write(heroes[1].name)
+		for h in players:
+			self.response.write("<div>" + h.name + " -- "  + "</div>")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+
+class Person(object):
+	def __init__(self,pin):
+		self.name = ""
+		self.text = 0
+		self.minutes = 0
+		self.internet = 0
+		self.__password = pin
+		
+		
+	@property 
+	def password(self):
+		return self.__password	
