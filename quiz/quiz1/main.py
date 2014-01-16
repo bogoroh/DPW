16d9b0e2587c17f1e75d59d8ecf0c463a4908ea9
bogoroh <mtaatgen@live.com>
2014-01-15 22:19:05 -0500

    C - Compled the first part for Quiz 1
    C = Completed

--- quiz/quiz1/main.py
+++ quiz/quiz1/main.py
@@ -7,8 +7,21 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+		self.response.write('Hello world!')
+		print calcArea(20,20)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+def calcArea (w,h):
+	area = str(w * h)
+
+	if (w == h):
+		areaMessage = "The area of your square is " + area + " square feet."
+		return areaMessage
+	else:
+		areaMessage = "The area of your rectangle is " + area + " square feet."
+		return areaMessage
+
+		
