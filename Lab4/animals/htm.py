8bb3a4b041c2f1fa235f9999e8fdf2bdba17c236
bogoroh <mtaatgen@live.com>
2014-01-20 14:41:27 -0500

    F - Added header + form
    added the template for header and form

---
+++ Lab4/animals/htm.py
@@ -0,0 +1,17 @@
+class Page():
+    def __init__(self):
+            self.__header = '''
+<!DOCTYPE>
+<html>
+    <head>
+            <title>Mike Taatgen Lab 4</title>
+            <link rel='stylesheet' href='css/main.css' />
+</head>
+    <body>
+            <div id="wrapper">'''
+            self.__form='''
+                <nav>
+                        <a href='/?animal=0'><button>Black Dolphins</button></a>
+                        <a href='/?animal=1'><button>Leopard Shark</button></a>
+                        <a href='/?animal=2'><button>Blue Marlin</button></a>
+                </nav>'''
