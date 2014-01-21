2499983bd213b9a3ac2ea57430e60134c1e34f66
bogoroh <mtaatgen@live.com>
2014-01-20 23:27:09 -0500

    A - Land class + Copyright
    Added the Land class plus copyrights on top of the page

---
+++ quiz/Quiz2/main.py
@@ -0,0 +1,35 @@
+# Mike Taatgen
+# DPW
+# 01-20-14
+# Quiz 2
+
+class Land(object):
+	def __init__(self):
+		def populationCheck(self):
+			people = 7023442934239123 # Amount of people on land
+			rateIncrease = 4.5 # Rate increase of the amount of people since last year
+
+			return 		
+		def populationGrowth(c,p):
+			newPopulation = c * p
+			print newPopulation
+			return newPopulation
+    self._age = 0 # Age of the land
+    self._location = "" # Location where the land is
+
+    @property
+    def location(self):
+        return self._location
+    @location.setter
+    def location(self, value):
+        self._location = value
+
+    @property
+    def age(self):
+        return self._age
+    @age.setter
+    def age(self, value):
+        self._age = value
+    
+class Island(Land)
+
