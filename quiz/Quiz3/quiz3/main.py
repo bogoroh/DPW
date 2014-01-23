5ca0478feac6dab05fa03fc65b5d71981c88cf15
bogoroh <mtaatgen@live.com>
2014-01-22 22:50:28 -0500

    A - 2methods
    Added 2 methods to the abstract class

--- quiz/Quiz3/quiz3/main.py
+++ quiz/Quiz3/quiz3/main.py
@@ -14,12 +14,22 @@ app = webapp2.WSGIApplication([
 
 class Sport(object):
 	def __init__(self):
-		pass
+		def Surface(self):
+			pass
+		def PrintClassName(self):
+			print "We're now inside the Abstract Class called: 'Sport' "	
 
 class Wakeboard(Sport):
 	def __init__(self):
 		super(Wakeboard, self).__init__()
+		def Surface(self):
+			print "This sport is played on the water"
+		def PrintClassName(self):
+			print "We're now inside the super class called: 'Wakeboard' "
 
 class Snowboard(Sport):
 	def __init__(self):
 		super(Snowboard, self).__init__()
+			print "This sport is played on snow"
+		def PrintClassName(self):
+			print "We're now inside the super Class called: 'Snowboard' "
