44e7b8db28a2c77713c0c8b31b480b18cc708bdf
bogoroh <mtaatgen@live.com>
2014-01-22 23:08:56 -0500

    A - Properties and Attributes

--- quiz/Quiz3/quiz3/main.py
+++ quiz/Quiz3/quiz3/main.py
@@ -14,14 +14,33 @@ app = webapp2.WSGIApplication([
 
 class Sport(object):
 	def __init__(self):
+		self.__age = ''
+		self.__inventory = ''
+		self._location = ''
+
+		@property
+		def age(self):
+		    return self.__age
+
+		@property
+		def inventory(self):
+		    return self.__inventory    
+		
 		def Surface(self):
 			pass
 		def PrintClassName(self):
-			print "We're now inside the Abstract Class called: 'Sport' "	
+			print "We're now inside the Abstract Class called: 'Sport' "
+
 
 class Wakeboard(Sport):
 	def __init__(self):
 		super(Wakeboard, self).__init__()
+		self.__inventory = 'Wakerope, Wakeboard, Boat'
+		self._location = 'Willemstad, Curacao'
+		
+		
+		
+
 		def Surface(self):
 			print "This sport is played on the water"
 		def PrintClassName(self):
@@ -30,6 +49,14 @@ class Wakeboard(Sport):
 class Snowboard(Sport):
 	def __init__(self):
 		super(Snowboard, self).__init__()
+		self.__inventory = 'Snowboard, Snowboots, Snow , Mountain'
+		self._location = 'Vancoucer, BC'
+
+		@property
+		def inventory(self):
+		    return self.__inventory   
+	        
+		def Surface(self):
 			print "This sport is played on snow"
 		def PrintClassName(self):
 			print "We're now inside the super Class called: 'Snowboard' "
