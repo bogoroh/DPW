383a2a8ef529ce94493291e1405fcbbb05d2e5f5
bogoroh <mtaatgen@live.com>
2014-01-28 12:50:19 -0500

    Got the information on the screen
    Got the information on the screen when i manually added ut after
    self.__station. Trying to figure out how to get it without that

--- Application/nsapi/HTMLLibrary.py
+++ Application/nsapi/HTMLLibrary.py
@@ -1,8 +1,53 @@
 class Page(object):
 	def __init__(self):
-		pass
+			self.title = "Main"
+			self.css = '<link rel="stylesheet" href="css/main.css" type="text/css" />'
+			self._header ='''<!DOCTYPE>
+<html>
+    <head>
+        <title>{self.title}</title>
+        {self.css}
+    </head>
+    <body>
+       '''   
+			self.__closer = '''
+    </body>
+</html>'''
+	
+	@property
+	def header(self):
+		return self._header
+	
+	@property	
+	def close(self):
+		return self.__closer
+
+	def update(self):
+		self._header = self._header.format(**locals())
 
 class Form(Page):
+	# name #Type #labels
+	#{"name":"zip","type":"text","label":"Enter your zipcode"}
 	def __init__(self,obj):
 		super(Form,self).__init__()
-		pass
+		self.method = "GET"
+		self.action = ""
+		self.__formOpen = '''
+<form action="{self.action}" method="{self.method}">'''
+
+		self.__inputs = ''
+		for el in obj:
+			print el['name']
+			if el['type'] == "text":
+				self.__inputs += '<input type="' + el['type']  + '" placeholder="' + el['label'] +'" name="' + el['name'] + '"></input>'
+			else:
+				self.__inputs += '<input type="' + el['type']  + '" value="' + el['label']  +'" name="' + el['name'] + '"></input>'	
+		self.__formClose = "</form>"
+
+	@property
+	def getForm(self):
+	    return self.__formOpen + self.__inputs + self.__formClose
+	
+	def update(self):
+		self._header = self._header.format(**locals())
+		self.__formOpen = self.__formOpen.format(**locals())
--- Application/nsapi/main.py
+++ Application/nsapi/main.py
@@ -15,8 +15,7 @@ class MainHandler(webapp2.RequestHandler):
 		self.__url = "http://webservices.ns.nl/ns-api-avt?station="
 		self.__station = "ut"
 		self.__req = urllib2.Request(self.__url + self.__station)
-
-
+		
 		#Creates authentication helper
 		password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
 		#Binds username and pass to url
@@ -24,15 +23,29 @@ class MainHandler(webapp2.RequestHandler):
 		#Stores user and pass to library's authentication helper
 		auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
 
-
 		#creates "openeer object for fetching page info"
-
 		self.__opener = urllib2.build_opener(auth_manager)
 		urllib2.install_opener(self.__opener)
 		#requests and brings back page info
 		handler = urllib2.urlopen(self.__req)
 		#prints out to the page
 		self.response.write(handler.read())
+
+		page = Page()
+		form_settings = [{"name":"station","type":"text","label":"Enter the abrevation of the station "},{"name":"submit","type":"submit","label":"Get departure times"}]
+		form = Form(form_settings)
+		form.update()
+		self.response.write(form.header + form.getForm + form.close)
+		if self.request.GET:
+			self.__station = self.request.GET['station']
+
+
+			openerURL = urllib2.build_opener() #magic to load request - creates framework to get url
+			result = openerURL.open(self.__req) # gets url and puts result in "result"
+			#print result
+			xmldoc = minidom.parse(result) #parse through string to get XML object 
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
