da8bc51e65b37e508f008812ddff6ea5168e0cab
bogoroh <mtaatgen@live.com>
2014-01-29 13:07:33 -0500

    A - Comments
    Added Comments to make it more readable

--- Application/nsapi/main.py
+++ Application/nsapi/main.py
@@ -10,10 +10,10 @@ from xml.dom import minidom #convert XML into an object
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-		self.__user = 'mtaatgen@live.com'
-		self.__pass = "dv8kZ24iGNZwFiHdZrClS_vaNCKTz1rY5jO9GckIj-fgVEqcgpcyLA"
-		self.__url = "http://webservices.ns.nl/ns-api-avt?station="
-		self.__station = "ut"
+		self.__user = 'mtaatgen@live.com' #Authenticator username
+		self.__pass = "dv8kZ24iGNZwFiHdZrClS_vaNCKTz1rY5jO9GckIj-fgVEqcgpcyLA" #Authenticator key
+		self.__url = "http://webservices.ns.nl/ns-api-avt?station=" #URL from the API
+		self.__station = "ut" #Had to start with something atleast for it now to show ifnot i was getting range errors in the code
 		self.__req = urllib2.Request(self.__url + self.__station)
 		
 		#Creates authentication helper
@@ -40,17 +40,16 @@ class MainHandler(webapp2.RequestHandler):
 			self.__station = self.request.GET['station']
 			openerURL = urllib2.build_opener() #magic to load request - creates framework to get url
 			result = openerURL.open(self.__req) # gets url and puts result in "result"
-			#print result
 			xmldoc = minidom.parse(result) #parse through string to get XML object 
 			content = '<br/>' 
 			ARit = xmldoc.getElementsByTagName('RitNummer')
-			AVertrekTijd = xmldoc.getElementsByTagName('VertrekTijd')
-			AEind = xmldoc.getElementsByTagName('EindBestemming')
-			ATrein = xmldoc.getElementsByTagName('TreinSoort')
-			AVertrek = xmldoc.getElementsByTagName('VertrekSpoor')
+			AVertrekTijd = xmldoc.getElementsByTagName('VertrekTijd') # Saved the departure time for that station into an array
+			AEind = xmldoc.getElementsByTagName('EindBestemming') # Saves all the final destination into an array  for that station
+			ATrein = xmldoc.getElementsByTagName('TreinSoort') # Saves all the Traintypes in an array for that station
+			AVertrek = xmldoc.getElementsByTagName('VertrekSpoor') #Saves all the Departure railways for that station into an array 
 			self.response.write(AVertrek[0].firstChild.nodeValue)
-			for l,m,n,o,p in zip(ARit, AVertrekTijd,AEind,ATrein,AVertrek):
-				content += 'Trainnumber: ' + l.firstChild.nodeValue
+			for l,m,n,o,p in zip(ARit, AVertrekTijd,AEind,ATrein,AVertrek): # Makes it possible to loop throught multiple arrays
+				content += 'Trainnumber: ' + l.firstChild.nodeValue 
 				content += 'Departure Time: ' + m.firstChild.nodeValue
 				content += 'Final Destination: ' + n.firstChild.nodeValue
 				content += 'Traintype: ' + o.firstChild.nodeValue
