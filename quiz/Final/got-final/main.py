c20576d3031008dc83b35e14966684c0c634e55e
bogoroh <mtaatgen@live.com>
2014-01-29 20:15:16 -0500

    Revert "A - Names displaying with breaks"
    This reverts commit 39f430e5ef9524eb9342ffd4c53c287f540552e7.
    
    Conflicts:
    	quiz/Final/got-final/main.py
    	quiz/Final/got-final/main.pyc

--- quiz/Final/got-final/main.py
+++ quiz/Final/got-final/main.py
@@ -1,4 +1,4 @@
-# Mike Taatgen
+# MIke Taatgen
 # DPW
 # Final
 # Game of Thrones
@@ -9,6 +9,7 @@ from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+<<<<<<< HEAD
     	GotModel()
 
 class GotModel(object):
@@ -26,6 +27,17 @@ class GotView(object):
 			content += "</br>"
 		self.response.write(content)
 
+=======
+        self.response.write('Hello world!')
+        file = open("GameOfThrones.xml",'r') # To read the local file
+        xml = minidom.parseString(file.read()) #to convert the file into an xml object
+        #dom = xml.minidom.parse("GameOfThrones.xml")
+        topic = xml.getElementsByTagName("name")
+        content = " </br>"
+        for l in topic:
+        	content += l.firstChild.nodeValue
+       	self.response.write(content)
+>>>>>>> parent of 39f430e... A - Names displaying with breaks
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
