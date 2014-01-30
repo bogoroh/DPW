39f430e5ef9524eb9342ffd4c53c287f540552e7
bogoroh <mtaatgen@live.com>
2014-01-29 20:04:19 -0500

    A - Names displaying with breaks
    Got the names displaying with breaks now what??????

--- quiz/Final/got-final/main.py
+++ quiz/Final/got-final/main.py
@@ -1,4 +1,4 @@
-# MIke Taatgen
+# Mike Taatgen
 # DPW
 # Final
 # Game of Thrones
@@ -11,14 +11,13 @@ from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
         file = open("GameOfThrones.xml",'r') # To read the local file
         xml = minidom.parseString(file.read()) #to convert the file into an xml object
-        #dom = xml.minidom.parse("GameOfThrones.xml")
-        topic = xml.getElementsByTagName("name")
-        content = " </br>"
-        for l in topic:
+        FList = xml.getElementsByTagName("name")
+        content = ""
+        for l in FList:
         	content += l.firstChild.nodeValue
+        	content += "</br>"
        	self.response.write(content)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
