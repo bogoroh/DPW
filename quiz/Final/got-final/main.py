8086afe95b17be4cd022fe94429bcd7b669552bf
bogoroh <mtaatgen@live.com>
2014-01-29 19:21:09 -0500

    A - GAE + XML
    Added the google app engine and the xml inside the folder i will be
    working on

---
+++ quiz/Final/got-final/main.py
@@ -0,0 +1,14 @@
+# MIke Taatgen
+# DPW
+# Final
+# Game of Thrones
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
