3faa520aabb1d8aa11b60f36369c2554d4af1233
bogoroh <mtaatgen@live.com>
2014-01-29 20:14:21 -0500

    Tried to get MVC working
    See you next month

--- quiz/Final/got-final/main.py
+++ quiz/Final/got-final/main.py
@@ -7,18 +7,25 @@ import webapp2
 import urllib2
 from xml.dom import minidom
 
-
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        file = open("GameOfThrones.xml",'r') # To read the local file
-        xml = minidom.parseString(file.read()) #to convert the file into an xml object
-        FList = xml.getElementsByTagName("name")
-        content = ""
-        for l in FList:
-        	content += l.firstChild.nodeValue
-        	content += "</br>"
-       	self.response.write(content)
+    	GotModel()
+
+class GotModel(object):
+	def __init__(self):
+		file = open("GameOfThrones.xml",'r') # To read the local file
+		xml = minidom.parseString(file.read()) #to convert the file into an xml object
+		FList = xml.getElementsByTagName("name")
+		x = GotView(FList)
+
+class GotView(object):
+	def __init__(self,FList):
+		content = ""
+		for l in FList:
+			content += l.firstChild.nodeValue
+			content += "</br>"
+		self.response.write(content)
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
