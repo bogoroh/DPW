29c2062e505b9480691546e951002b04e86ca40f
bogoroh <mtaatgen@live.com>
2014-01-29 19:55:19 -0500

    A- Got the titles showing up
    Got the titles showing up on the page

--- quiz/Final/got-final/main.py
+++ quiz/Final/got-final/main.py
@@ -4,11 +4,22 @@
 # Game of Thrones
 
 import webapp2
+import urllib2
+from xml.dom import minidom
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
-
+        file = open("GameOfThrones.xml",'r') # To read the local file
+        xml = minidom.parseString(file.read()) #to convert the file into an xml object
+        #dom = xml.minidom.parse("GameOfThrones.xml")
+        topic = xml.getElementsByTagName("name")
+        content = " </br>"
+        for l in topic:
+        	content += l.firstChild.nodeValue
+       	self.response.write(content)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
