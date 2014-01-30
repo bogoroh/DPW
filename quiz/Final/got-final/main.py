cdbd5c1af24742fed06a91fa327479f6d2b2b2a1
bogoroh <mtaatgen@live.com>
2014-01-29 20:15:31 -0500

    Revert 39f430e..c20576d
    This rolls back to commit 39f430e5ef9524eb9342ffd4c53c287f540552e7.

--- quiz/Final/got-final/main.py
+++ quiz/Final/got-final/main.py
@@ -1,4 +1,4 @@
-# MIke Taatgen
+# Mike Taatgen
 # DPW
 # Final
 # Game of Thrones
@@ -7,37 +7,18 @@ import webapp2
 import urllib2
 from xml.dom import minidom
 
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-<<<<<<< HEAD
-    	GotModel()
-
-class GotModel(object):
-	def __init__(self):
-		file = open("GameOfThrones.xml",'r') # To read the local file
-		xml = minidom.parseString(file.read()) #to convert the file into an xml object
-		FList = xml.getElementsByTagName("name")
-		x = GotView(FList)
-
-class GotView(object):
-	def __init__(self,FList):
-		content = ""
-		for l in FList:
-			content += l.firstChild.nodeValue
-			content += "</br>"
-		self.response.write(content)
-
-=======
-        self.response.write('Hello world!')
         file = open("GameOfThrones.xml",'r') # To read the local file
         xml = minidom.parseString(file.read()) #to convert the file into an xml object
-        #dom = xml.minidom.parse("GameOfThrones.xml")
-        topic = xml.getElementsByTagName("name")
-        content = " </br>"
-        for l in topic:
+        FList = xml.getElementsByTagName("name")
+        content = ""
+        for l in FList:
         	content += l.firstChild.nodeValue
+        	content += "</br>"
        	self.response.write(content)
->>>>>>> parent of 39f430e... A - Names displaying with breaks
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
